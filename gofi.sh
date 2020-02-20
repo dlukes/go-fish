bf0d13c20a681568f54b81d44c035834f0fbfeed
David Lukes <dafydd.lukes@gmail.com>
2020-02-20 10:11:16 +0100

    Refactor gofi.sh

--- gofi.sh
+++ gofi.sh
@@ -12,9 +12,12 @@
 set -e
 
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+
+# where to put programs not installed from .deb packages
 prefix=/usr/local
 mandir="$prefix/share/man/man1"
 bindir="$prefix/bin"
+
 fish_conf_dir="$HOME/.config/fish"
 fonts_confd=/etc/fonts/conf.d
 fontconfig_overrides="$fonts_confd/00-overrides.conf"
@@ -23,42 +26,60 @@ fontconfig_overrides="$fonts_confd/00-overrides.conf"
 # Functions
 #-----------------------------------------------------------------------
 
+width="$( seq 1 72 )"
 log() {
-  msg="$1"
-  color="$2"
+  local msg="$1"
+  local color="$2"
+  local sep="$3"
   if [ -n "$color" ]; then
-    reset='[0m'
+    reset='\e[0m'
   fi
-  >&2 echo "$color$msg$reset"
+  if [ -n "$sep" ]; then
+    sep=$( printf "$sep%.s" $width )
+    before="$sep\n\n"
+    after="\n\n$sep"
+  fi
+  >&2 printf "$color$before$msg$after$reset\n"
 }
 
 info() {
-  msg="$1"
-  log "$msg" '[32m'
+  local msg="$1"
+  local sep="$2"
+  log "$msg" '\e[32m' "$sep"
 }
 
 warning() {
-  msg="$1"
-  log "$msg" '[33m'
+  local msg="$1"
+  local sep="$2"
+  log "$msg" '\e[33m' "$sep"
 }
 
 error() {
-  msg="$1"
-  log "$msg" '[31m'
+  local msg="$1"
+  local sep="$2"
+  log "$msg" '\e[31m' "$sep"
 }
 
-fetch_and_install_deb() {
-  cmdname="$1"
-  repo="$2"
-  archive_regex="$3"
-  if ! command -v "$cmdname" >/dev/null || [ -n "$GOFISH_FORCE" ]; then
+# Fetch installation archive if cmd is not available, else exit 2.
+maybe_fetch_archive() {
+  local cmd="$1"
+  local repo="$2"
+  local archive_regex="$3"
+  if ! command -v "$cmd" >/dev/null || [ -n "$GOFISH_FORCE" ]; then
     release_link=https://github.com$(
       curl -sL "https://github.com/$repo/releases" |
         grep -oPm1 '/[^"]+/'"$archive_regex"
     )
     curl -sLO "$release_link"
-    sudo dpkg --force-overwrite -i $( basename "$release_link" )
+    basename "$release_link"
+  else
+    exit 2
   fi
+}
+
+maybe_install_deb() {
+  local deb=$( maybe_fetch_archive "$@" ) &&
+    sudo dpkg --force-overwrite -i "$deb"
 }
 
 #-----------------------------------------------------------------------
@@ -107,18 +128,14 @@ if [ -d "$fish_conf_dir" ]; then
   disabled="$fish_conf_dir:disabled:$(date +%s)"
   set +x
   warning "\
-========================================================================
-
 Moving existing fish configuration directory:
 
 $fish_conf_dir
 
 to:
 
-$disabled
-
-========================================================================\
-"
+$disabled\
+" =
   set -x
   mv "$fish_conf_dir" "$disabled"
 fi
@@ -143,95 +160,90 @@ fi
 workdir=$( mktemp -d )
 cd "$workdir"
 
+set +x
+info "\
+Temporary working directory is:
+
+$workdir
+
+All downloaded and extracted files will be placed there, so that's where
+you should check if anything goes wrong.\
+" =
+set -x
+
 #-----------------------------------------------------------------------
 # Install fzf (for fuzzy-searching all the things™):
 #-----------------------------------------------------------------------
 
-if ! command -v fzf >/dev/null || [ -n "$GOFISH_FORCE" ]; then
-  fzf_release_link=https://github.com$(
-    curl -sL https://github.com/junegunn/fzf-bin/releases |
-      grep -oPm1 '/[^"]+/fzf-.*?-linux_amd64.tgz'
-  )
-  curl -sLO "$fzf_release_link"
-  tar xzf $( basename "$fzf_release_link" )
-  sudo mv fzf "$bindir"
-
-  # additional resources are only in the source release, so it has to be
-  # fetched separately
-  fzf_source_release_link=https://github.com$(
-    curl -sL https://github.com/junegunn/fzf/releases |
-      grep -oPm1 '/[^"]+\.tar\.gz'
-  )
-  curl -sLO "$fzf_source_release_link"
-  tar xzf $( basename "$fzf_source_release_link" )
+# additional resources are only in the source release, so it has to be
+# fetched separately and *first*, because once the fzf binary is
+# available, the command -v check in maybe_fetch_archive will succeed
+# and the conditional block will be skipped
+fzf_source_archive=$( maybe_fetch_archive fzf junegunn/fzf '.*?\.tar\.gz' ) && {
+  tar xzf "$fzf_source_archive"
   sudo mv fzf-*/man/man1/fzf.1 "$mandir"
   fzf_share="$prefix/share/fzf"
   sudo rm -rf "$fzf_share"
   sudo mkdir -p "$fzf_share"
   sudo mv fzf-*/shell "$fzf_share"
-
   update_man_db=1
-fi
+}
+
+fzf_binary_archive=$( maybe_fetch_archive fzf junegunn/fzf-bin 'fzf-.*?-linux_amd64.tgz' ) && {
+  tar xzf "$fzf_binary_archive"
+  sudo mv fzf "$bindir"
+}
 
 #-----------------------------------------------------------------------
 # Install fasd (for quick cd based on frecency of history entries)
 #-----------------------------------------------------------------------
 
-if ! command -v fasd >/dev/null || [ -n "$GOFISH_FORCE" ]; then
-  fasd_release_link=https://github.com$(
-    curl -sL https://github.com/clvv/fasd/releases |
-      grep -oPm1 '/[^"]+\.tar\.gz'
-  )
-  curl -sLO "$fasd_release_link"
-  tar xzf $( basename "$fasd_release_link" )
+fasd_archive=$( maybe_fetch_archive fasd clvv/fasd '.*?\.tar\.gz' ) && {
+  tar xzf "$fasd_archive"
   sudo mv fasd-*/fasd.1 "$mandir"
   sudo mv fasd-*/fasd "$bindir"
-
   update_man_db=1
-fi
+}
 
 #-----------------------------------------------------------------------
 # Install exa (a modern replacement for ls, with git support & more):
 #-----------------------------------------------------------------------
 
-if ! command -v exa >/dev/null || [ -n "$GOFISH_FORCE" ]; then
-  exa_release_link=https://github.com$(
-    curl -sL https://github.com/ogham/exa/releases |
-      grep -oPm1 '/[^"]+/exa-linux-x86_64-.*?\.zip'
-  )
-  curl -sLO "$exa_release_link"
-  unzip $( basename "$exa_release_link" )
-  sudo mv exa-linux-x86_64 "$bindir/exa"
+repo=ogham/exa
 
-  # additional resources are only in the source release, so it has to be
-  # fetched separately
-  exa_source_release_link=https://github.com$(
-    curl -sL https://github.com/ogham/exa/releases |
-      grep -oPm1 '/[^"]+\.tar\.gz'
-  )
-  curl -sLO "$exa_source_release_link"
-  tar xzf $( basename "$exa_source_release_link" )
+# additional resources are only in the source release, so it has to be
+# fetched separately and *first*, because once the exa binary is
+# available, the command -v check in maybe_fetch_archive will succeed
+# and the conditional block will be skipped
+exa_source_archive=$( maybe_fetch_archive exa "$repo" '.*?\.tar\.gz' ) && {
+  tar xzf "$exa_source_archive"
   sudo mv exa-*/contrib/man/exa.1 "$mandir"
-  mv exa-*/contrib/completions.fish "$fish_conf_dir/completions/exa.fish"
-fi
+  sudo mv exa-*/contrib/completions.fish /usr/share/fish/completions/exa.fish
+  update_man_db=1
+}
+
+exa_binary_archive=$( maybe_fetch_archive exa "$repo" 'exa-linux-x86_64-.*?\.zip' ) && {
+  unzip "$exa_binary_archive"
+  sudo mv exa-linux-x86_64 "$bindir/exa"
+}
 
 #-----------------------------------------------------------------------
 # Install bat (a cat(1) clone with wings):
 #-----------------------------------------------------------------------
 
-fetch_and_install_deb bat sharkdp/bat 'bat_.*?amd64.deb'
+maybe_install_deb bat sharkdp/bat 'bat_.*?amd64.deb'
 
 #-----------------------------------------------------------------------
 # Install rg (a faster grep clone with better defaults):
 #-----------------------------------------------------------------------
 
-fetch_and_install_deb rg BurntSushi/ripgrep 'ripgrep_.*?_amd64.deb'
+maybe_install_deb rg BurntSushi/ripgrep 'ripgrep_.*?_amd64.deb'
 
 #-----------------------------------------------------------------------
 # Install fd (a faster & simpler find clone with better defaults):
 #-----------------------------------------------------------------------
 
-fetch_and_install_deb fd sharkdp/fd 'fd_.*?_amd64.deb'
+maybe_install_deb fd sharkdp/fd 'fd_.*?_amd64.deb'
 
 #-----------------------------------------------------------------------
 # Fix Ubuntu Mono fontconfig substitution rules (prompt uses Unicode)
@@ -240,38 +252,28 @@ fetch_and_install_deb fd sharkdp/fd 'fd_.*?_amd64.deb'
 if [ ! -d "$fonts_confd" ]; then
   set +x
   warning "\
-========================================================================
-
 The directory
 
 $fonts_confd
 
 does not exist, will not attempt to fix Ubuntu Mono font substitution
-rules.
-
-========================================================================\
-"
+rules.\
+" =
   set -x
 elif [ -e "$fontconfig_overrides" ]; then
   set +x
   warning "\
-========================================================================
-
 A file named
 
 $fontconfig_overrides
 
 already exists; refusing to overwrite it to fix Ubuntu Mono font
-substitution rules.
-
-========================================================================\
-"
+substitution rules.\
+" =
   set -x
 else
   set +x
   info "\
-========================================================================
-
 Installing fontconfig substitution overrides for Ubuntu Mono. This is
 because the default prompt uses Unicode characters which don't exist in
 Ubuntu Mono and get substituted by a sans-serif font by default because
@@ -289,10 +291,8 @@ instead. Cf.:
 https://github.com/dlukes/go-fish#troubleshooting-unicode-glyphs-in-prompt
 
 NOTE: You may need to run 'fc-cache -fv' to update your fontconfig cache
-and/or open a new terminal window for these changes to take effect.
-
-========================================================================\
-"
+and/or open a new terminal window for these changes to take effect.\
+" =
   set -x
   cat <<'END' | sudo tee "$fontconfig_overrides"
 <?xml version="1.0"?>
@@ -326,17 +326,9 @@ fi
 
 set +x
 info "\
-========================================================================
-
-All done. Temporary working directory was:
-
-$workdir
-
-All downloaded and extracted files are there, if you'd like to check
-something.
-
-Invoke the fish command to try the fish shell out and see if you like
-it. Check out the README for tips on what's available and configured:
+All done. Invoke the 'fish' command to try the fish shell out and see if
+you like it. Check out the README for tips on what's available and
+configured:
 
 https://github.com/dlukes/go-fish
 
@@ -344,5 +336,5 @@ And the fish shell website for a tutorial and extensive documentation:
 
 https://fishshell.com/
 
-Have fun with the Friendly Interactive SHell!\
-"
+Have fun using the Friendly Interactive SHell!\
+" =
